3f66dffe145e00e27f9d87a779c63a91474a447a
Ahmed Hajeyah <hajeyah@gmail.com>
2022-06-27 20:23:24 +0300

    Signup / Image Picker / Logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bankofcoded/pages/home_page.dart';
 import 'package:bankofcoded/pages/signin.dart';
+import 'package:bankofcoded/pages/signup.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return MaterialApp(
-      home: SignInPage(),
+      debugShowCheckedModeBanner: false,
+      home: SignUpPage(),
     );
   }
 }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,39 +1,78 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpPage extends StatelessWidget {
   SignUpPage({Key? key}) : super(key: key);
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+  var _image;
+  final _picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Sign In"),
+        title: const Text("Sign Up"),
       ),
       body: Center(
-        child: Column(
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Username",
+        child: Container(
+          margin: EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/logo.png",
+                height: 200,
+                width: 200,
               ),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Password",
+              GestureDetector(
+                onTap: () async {
+                  final XFile? image =
+                      await _picker.pickImage(source: ImageSource.gallery);
+                  setState(() {
+                    _image = File(image!.path);
+                  });
+                },
+                child: CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.grey,
+                  backgroundImage:
+                      _image == null ? null : Image.file(_image).image,
+                ),
               ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Signup"),
-            ),
-          ],
+              Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                  ),
+                  child: Text("Please Sign Up")),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: "Username",
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: "Password",
+                ),
+                obscureText: true,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Signup"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  void getImage(BuildContext context) {}
+
+  void setState(Null Function() param0) {}
 }
