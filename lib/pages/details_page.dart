338367d7ac484e414a7ce1eaff970ea269a81812
MajedALRamdan <majooddark237514@gmail.com>
2022-06-30 16:33:04 +0300

    userprofile page done

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -29,41 +29,44 @@ class DetailsPage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(15.0),
                 ),
                 color: Color.fromARGB(39, 241, 240, 240).withOpacity(1),
-                child: Row(children: [
-                  Container(
-                      margin: EdgeInsets.only(left: 10),
-                      child: CircleAvatar(
-                        radius: 55.0,
-                        backgroundColor: Colors.transparent,
-                        child: ClipRRect(
-                          child: Image.asset('assets/images/logo.png'),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      )),
-                  SizedBox(
-                    width: 70,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 75,
-                    ),
-                    child: Container(
-                      margin: EdgeInsets.only(
-                        bottom: 30,
+                child: Column(
+                  children: [
+                    Row(children: [
+                      Container(
+                          margin: EdgeInsets.only(left: 10, top: 10),
+                          child: CircleAvatar(
+                            radius: 55.0,
+                            backgroundColor: Colors.transparent,
+                            child: ClipRRect(
+                              child: Image.asset('assets/images/logo.png'),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          )),
+                      SizedBox(
+                        width: 70,
                       ),
-                      child: Column(
+                      Column(
                         children: [
                           Container(
-                            child: Text("welcome"),
-                          ),
-                          Container(
-                            child: Text("name"),
+                            margin: EdgeInsets.only(
+                              top: 25,
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Text("welcome"),
+                                ),
+                                Container(
+                                  child: Text("name"),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  ),
-                ]),
+                    ]),
+                  ],
+                ),
               ),
             ),
             Container(
@@ -73,50 +76,79 @@ class DetailsPage extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("My Balance: "),
+                  Text(
+                    "My Balance: ",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
                   Container(
                       padding: EdgeInsets.only(
                         left: 10,
                       ),
-                      child: Text("amount"))
+                      child: Text(
+                        "amount",
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ))
                 ],
               ),
             ),
             Container(
-              margin: EdgeInsets.only(top: 30, left: 35),
+              margin: EdgeInsets.only(
+                top: 30,
+              ),
               child: Container(
                 margin: EdgeInsets.only(
                   top: 30,
                 ),
                 child: Center(
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Center(
-                        child: Container(
-                          child: Column(
-                            children: [
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                    shape: const CircleBorder(),
-                                    primary: Color.fromARGB(255, 23, 159, 41)),
-                                child: Container(
-                                  width: 90,
-                                  height: 90,
-                                  alignment: Alignment.center,
-                                  decoration: const BoxDecoration(
-                                      shape: BoxShape.circle),
-                                  child: Center(
-                                    child: const Text(
-                                      'Deposite',
-                                      style: TextStyle(fontSize: 20),
-                                    ),
-                                  ),
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: const CircleBorder(),
+                                primary: Color.fromARGB(255, 23, 159, 41)),
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              alignment: Alignment.center,
+                              decoration:
+                                  const BoxDecoration(shape: BoxShape.circle),
+                              child: Center(
+                                child: const Text(
+                                  'Deposite',
+                                  style: TextStyle(fontSize: 20),
                                 ),
-                                onPressed: () {},
                               ),
-                            ],
+                            ),
+                            onPressed: () {},
                           ),
-                        ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: const CircleBorder(),
+                                primary: Colors.red),
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              alignment: Alignment.center,
+                              decoration:
+                                  const BoxDecoration(shape: BoxShape.circle),
+                              child: Center(
+                                child: const Text(
+                                  'Withdraw',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
                       ),
                       Container(
                         child: Column(
@@ -124,7 +156,7 @@ class DetailsPage extends StatelessWidget {
                             ElevatedButton(
                               style: ElevatedButton.styleFrom(
                                   shape: const CircleBorder(),
-                                  primary: Colors.red),
+                                  primary: Color.fromARGB(255, 25, 47, 216)),
                               child: Container(
                                 width: 90,
                                 height: 90,
@@ -133,7 +165,7 @@ class DetailsPage extends StatelessWidget {
                                     const BoxDecoration(shape: BoxShape.circle),
                                 child: Center(
                                   child: const Text(
-                                    'Withdraw',
+                                    'Transfer',
                                     style: TextStyle(fontSize: 20),
                                   ),
                                 ),
@@ -148,32 +180,34 @@ class DetailsPage extends StatelessWidget {
                 ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(
-                top: 75,
-              ),
-              child: Column(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        primary: Color.fromARGB(255, 25, 47, 216)),
-                    child: Container(
-                      width: 90,
-                      height: 90,
-                      alignment: Alignment.center,
-                      decoration: const BoxDecoration(shape: BoxShape.circle),
-                      child: Center(
-                        child: const Text(
-                          'Transfer',
-                          style: TextStyle(fontSize: 20),
+            Column(
+              children: [
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      top: 30,
+                    ),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          primary: Color.fromARGB(255, 220, 157, 29)),
+                      child: Container(
+                        width: 130,
+                        height: 120,
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(shape: BoxShape.circle),
+                        child: Center(
+                          child: const Text(
+                            'Transcation',
+                            style: TextStyle(fontSize: 20),
+                          ),
                         ),
                       ),
+                      onPressed: () {},
                     ),
-                    onPressed: () {},
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
